6740ae875e4115a8f2ee8248ab7a2ee7f385e197
felipematheus1337 <lipehbr@gmail.com>
2025-10-29 21:26:50 -0300

    Criado classe de configuração do kafka para ms de pedidos.

---
+++ pedidos/src/main/java/com/dogao/pedidos/config/KafkaConfig.java
@@ -0,0 +1,62 @@
+package com.dogao.pedidos.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${topics.pedido-criado}")
+    private String pedidoCriadoTopic;
+
+    @Value("${topics.pagamento-processado}")
+    private String pagamentoProcessadoTopic;
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public NewTopic pedidoCriadoTopic() {
+        return TopicBuilder.name(pedidoCriadoTopic)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic pagamentoProcessadoTopic() {
+        return TopicBuilder.name(pagamentoProcessadoTopic)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+}
